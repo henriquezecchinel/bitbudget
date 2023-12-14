6cecc012cec29f316d71445d232bb9ea34bf95d8
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-15 00:08:14 +0100

    Spring Initializr

---
+++ src/main/java/com/zecchinel/bitbudget/BitBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.zecchinel.bitbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zecchinel/bitbudget/BitBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zecchinel.bitbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
