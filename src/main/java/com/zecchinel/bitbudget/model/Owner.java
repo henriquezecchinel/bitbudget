6a6e77560e102b39537980fc634d3fb5f7c78ba0
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-23 19:06:49 +0100

    updated Owner to extend BaseEntity

--- src/main/java/com/zecchinel/bitbudget/model/Owner.java
+++ src/main/java/com/zecchinel/bitbudget/model/Owner.java
@@ -1,20 +1,7 @@
 package com.zecchinel.bitbudget.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Owner {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
-    @Column(unique = true, columnDefinition = "citext")
-    private String name;
+public class Owner extends BaseEntity {
 }
