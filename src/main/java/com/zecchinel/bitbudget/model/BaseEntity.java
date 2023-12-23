03053a554c0d3e5cb69415e0cf82657daed9af67
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-23 19:06:29 +0100

    added BaseEntity Class

---
+++ src/main/java/com/zecchinel/bitbudget/model/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.zecchinel.bitbudget.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(unique = true, columnDefinition = "citext")
+    private String name;
+}
