cc8eadf7f317a5356916bc9b34aa8cc499e18ca6
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-23 19:56:31 +0100

    added BaseEntityDto

---
+++ src/main/java/com/zecchinel/bitbudget/dto/BaseEntityDto.java
@@ -0,0 +1,14 @@
+package com.zecchinel.bitbudget.dto;
+
+import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@MappedSuperclass
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BaseEntityDto {
+    private String name;
+}
