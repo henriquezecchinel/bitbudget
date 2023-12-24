bb4cb649b6028f2c3b1fa5a1a3709d302bfca02f
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-24 15:04:40 +0100

    added TODO

--- src/main/java/com/zecchinel/bitbudget/model/SubCategory.java
+++ src/main/java/com/zecchinel/bitbudget/model/SubCategory.java
@@ -12,4 +12,7 @@ public class SubCategory extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "main_category_id", nullable = false)
     private MainCategory mainCategory;
+
+    // Since we can have multiple subcategories with the same name for different main categories:
+    // TODO: update the unique key to be (`name` + `main_category_id`) instead of (`name`)
 }
