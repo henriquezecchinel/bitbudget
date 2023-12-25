31b22fbee21074a45ebee01de5ef8080723957da
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-25 01:41:55 +0100

    refactored field `name` to `description`

--- src/main/java/com/zecchinel/bitbudget/dto/SubCategoryDto.java
+++ src/main/java/com/zecchinel/bitbudget/dto/SubCategoryDto.java
@@ -1,19 +1,15 @@
 package com.zecchinel.bitbudget.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class SubCategoryDto extends BaseEntityDto {
     private MainCategoryDto mainCategory;
 
-    public SubCategoryDto(Long id, String name, MainCategoryDto mainCategoryDto) {
-        super(id, name);
+    public SubCategoryDto(Long id, String description, MainCategoryDto mainCategoryDto) {
+        super(id, description);
         this.mainCategory = mainCategoryDto;
     }
 }
--- src/main/java/com/zecchinel/bitbudget/dto/TagDto.java
+++ src/main/java/com/zecchinel/bitbudget/dto/TagDto.java
@@ -6,7 +6,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class TagDto extends BaseEntityDto {
-    public TagDto(Long id, String name) {
-        super(id, name);
+    public TagDto(Long id, String description) {
+        super(id, description);
     }
 }
