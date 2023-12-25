c1b4101e214687d32b758aec2b12ddffda18e547
Henrique Zecchinel <henriquezecchinel@gmail.com>
2023-12-25 01:41:27 +0100

    fixed unique key

--- src/main/java/com/zecchinel/bitbudget/model/SubCategory.java
+++ src/main/java/com/zecchinel/bitbudget/model/SubCategory.java
@@ -1,18 +1,23 @@
 package com.zecchinel.bitbudget.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
+@Table(uniqueConstraints={@UniqueConstraint(columnNames = {"description", "main_category_id"})})
 public class SubCategory extends BaseEntity {
+
+    @Column(unique = false)
+    private String description;
 
     @ManyToOne
     @JoinColumn(name = "main_category_id", nullable = false)
     private MainCategory mainCategory;
-
-    // Since we can have multiple subcategories with the same name for different main categories:
-    // TODO: update the unique key to be (`name` + `main_category_id`) instead of (`name`)
 }
